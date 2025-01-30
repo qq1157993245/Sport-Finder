2a637a5f09814306d92737d12a9cb542b3d70564
Leo <zliu380@ucsc.edu>
2025-01-29 19:31:20 -0800

    login authentication set up

---
+++ app/(auth)/login.jsx
@@ -0,0 +1,52 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import {getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithRedirect } from "firebase/auth";
+
+//Normal login function. Needs to pass *email* and *password* as parameters.
+function loginAuth(email, password) {
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    const user = userCredential.user;
+    console.log(user);
+  })
+  .catch((error) => {
+    console.log(`error message: ${error}`);
+  });
+}
+
+//Google login function.
+function googleAuth() {
+    const provider = new GoogleAuthProvider();
+    const auth = getAuth();
+    signInWithRedirect(auth, provider);
+  
+    getRedirectResult(auth)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access Google APIs.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+  
+      // The signed-in user info.
+      const user = result.user;
+      // IdP data available using getAdditionalUserInfo(result)
+      // ...
+    }).catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.customData.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+    });
+}
+
+const login = () => {
+    
+}
+
+
+export default login
---
+++ app/config/firebaseConfig.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCfsElSve4AExvEjRz3ew3u2r5lRMiO4Eo",
+  authDomain: "sportsfinder-25b5f.firebaseapp.com",
+  projectId: "sportsfinder-25b5f",
+  storageBucket: "sportsfinder-25b5f.firebasestorage.app",
+  messagingSenderId: "619560730482",
+  appId: "1:619560730482:web:48b334337c0078191227eb",
+  measurementId: "G-N4ZGBBXZ6F"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
