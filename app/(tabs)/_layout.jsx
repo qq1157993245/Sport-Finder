9cd84cd10d6314f548c9c0bb6741ba0760f63276
Leo <zliu380@ucsc.edu>
2025-02-23 23:49:43 -0800

    update

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,6 +1,6 @@
 import { View, Text, Image } from 'react-native';
 import { Tabs } from 'expo-router';
-import { icons } from '../../constants';
+import {icons} from '../../constants';
 
 const TabIcon = ({ icon, color, name, focused }) => {
   return (
